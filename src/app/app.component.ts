1a9b5147b55f0bd08e15778b7cf3bdc608d40b76
Mateusz Lata <mateuszlata2@gmail.com>
2020-10-27 11:53:44 +0100

    Finish pagination functionality. Remake class switching with Angular directives.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentPage = 0;
+  
   images = [
     {
-      title: 'At the Beach',
-      url: ''
-    }
+      title: 'New York',
+      url: 'https://images.unsplash.com/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Las Vegas',
+      url: 'https://images.unsplash.com/photo-1549861833-c5932fd19229?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Los Angeles',
+      url: 'https://images.unsplash.com/photo-1515896769750-31548aa180ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Boston',
+      url: 'https://images.unsplash.com/photo-1501979376754-2ff867a4f659?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Chicago',
+      url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
   ];
 }
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
+import { Component, OnInit, Input } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-pagination',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
+  @Input() images: []; 
+  @Input() currentPage: number;
 
   constructor() { }
 
@@ -14,15 +18,49 @@ export class PaginationComponent implements OnInit {
 
   priClass = 'pag__item';
   secClass = 'pag__item--active';
-  
+  prevClass = 'pag__item--previous';
+  nextClass = 'pag__item--next';
+
+  removeClass(element: any, className: string) {
+    for(let i = 0; i <= element.parentElement.children.length-1; i++) {
+      element.parentElement.children[i].classList.remove(className);
+    }
+  }
 
   onClickPag(element: any) {
-    if(!element.classList.contains(this.secClass)) {
-      for(let i = 0; i <= element.parentElement.children.length-1; i++) {
-        element.parentElement.children[i].classList.remove(this.secClass);
-      }
-      element.classList.add(this.secClass);
-    }
-  } 
+    // Add active class after clicking - it is not needed anymore thanks to class directive
+    // if(!element.classList.contains(this.secClass) && (!element.classList.contains(this.prevClass) && !element.classList.contains(this.nextClass))) {
+    //   this.removeClass(element, this.secClass);
+    //   element.classList.add(this.secClass);
+    // }
 
+    // Next functionality
+    // let el = 0;
+    // if(element.classList.contains(this.nextClass)) {
+    //   for(let i = 0; i <= element.parentElement.children.length-1; i++) {
+    //     if(element.parentElement.children[i].classList.contains(this.secClass)) {
+    //       el = i + 1;
+    //     }
+    //   }
+    //   if(!element.parentElement.children[el].classList.contains(this.nextClass) && !element.parentElement.children[el].classList.contains(this.prevClass)) {
+    //     this.removeClass(element, this.secClass);
+    //     element.parentElement.children[el].classList.add(this.secClass)
+    //   }
+    // }
+
+    // Previous functionality
+    // el = 0;
+    // if(element.classList.contains(this.prevClass)) {
+    //   for(let i = 0; i <= element.parentElement.children.length-1; i++) {
+    //     if(element.parentElement.children[i].classList.contains(this.secClass)) {
+    //       el = i - 1;
+    //     }
+    //   }
+    //   if(!element.parentElement.children[el].classList.contains(this.prevClass) && !element.parentElement.children[el].classList.contains(this.nextClass)) {
+    //     this.removeClass(element, this.secClass);
+    //     element.parentElement.children[el].classList.add(this.secClass)
+    //   }
+    // }
+    console.log(this.currentPage);
+  }
 }
